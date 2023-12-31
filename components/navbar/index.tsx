579af842e9eeb326388d216b1b2563c0f0cd4fce
aad05 <abduvoitovasadbek3@gmail.com>
2023-12-31 21:10:36 +0500

    fixing: navbar (button)

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,12 +1,14 @@
 import { Languages, Moon } from "lucide-react";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { IconRenderer } from "../ui/icon-renderer";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center px-8 w-[80%] m-auto mt-[50px] h-[65px] bg-[#fff] rounded-[103px] shadow">
       <div className="flex items-center gap-2 select-none cursor-pointer">
         <Image
-          className="w-[40.07px] h-10"
+          className="w-[30px] h-[30px]"
           //   src="https://avatars.githubusercontent.com/u/150591042?s=48&v=4"
           src="/logo.png"
           width={40}
@@ -16,28 +18,21 @@ const Navbar = () => {
         <p className="text-neutral-600 text-xl font-bold">Scribblify</p>
       </div>
       <div className="flex items-center gap-8">
-        <div className="flex gap-4">
-          <p className="text-neutral-600 text-base font-medium cursor-pointer">
-            Tutorial
-          </p>
-          <p className="text-neutral-600 text-base font-medium cursor-pointer">
-            Prices
-          </p>
-          <p className="text-neutral-600 text-base font-medium cursor-pointer">
-            Gallery
-          </p>
+        <div className="flex">
+          <Button variant={"link"}>Tutorial</Button>
+          <Button variant={"link"}>Prices</Button>
+          <Button variant={"link"}>Gallery</Button>
         </div>
 
-        <Moon className="cursor-pointer" />
+        <IconRenderer>
+          <Moon className="cursor-pointer" />
+        </IconRenderer>
 
-        <Languages className="cursor-pointer" />
+        <IconRenderer>
+          <Languages className="cursor-pointer" />
+        </IconRenderer>
 
-        <div className="cursor-pointer w-fit h-[43px] px-6 py-3 bg-gradient-to-r from-blue-600 to-blue-300 rounded-[10px] justify-center items-center gap-2.5 inline-flex">
-          <div className="text-white text-base font-bold">Join</div>
-        </div>
-        <div className="cursor-pointer w-fit h-[43px] px-6 py-3 bg-white rounded-[10px] shadow justify-center items-center gap-2.5 inline-flex">
-          <div className="text-neutral-600 text-base font-bold">Sign Up</div>
-        </div>
+        <Button>Join</Button>
       </div>
     </div>
   );
---
+++ components/ui/icon-renderer.tsx
@@ -0,0 +1,45 @@
+import type { FC } from "react";
+
+type IconRendererProps = {
+  children: React.ReactElement;
+  onClick?: () => void;
+  className?: string;
+};
+
+type IconRendererWithTextType = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+};
+
+const IconRenderer: FC<IconRendererProps> = ({
+  children,
+  onClick,
+  className,
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`flex items-center justify-center w-[32px] h-[32px] p-1 cursor-pointer dark:hover:bg-[#323232] hover:bg-[#e8e8e8] transition-colors rounded-md ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+const IconRendererWithText: FC<IconRendererWithTextType> = ({
+  children,
+  onClick,
+  className,
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`flex items-center p-2 gap-2 h-[32px] cursor-pointer dark:hover:bg-[#323232] hover:bg-[#e8e8e8] transition-colors rounded-md ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { IconRendererWithText, IconRenderer };
