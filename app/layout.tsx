ba809a8adf3f4eb1e40216a2968a1f1081242f66
aad05 <abduvoitovasadbek3@gmail.com>
2024-01-01 22:33:51 +0500

    feat: theme-config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,14 +22,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased bg-zinc-100",
-          fontSans.variable
-        )}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
       >
-        {children}
-      </body>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased bg-zinc-100",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
